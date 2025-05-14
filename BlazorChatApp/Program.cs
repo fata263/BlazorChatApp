dbf12bbac004b90458a540d8313e3673017d6fcc
Karampanah Fatemeh (uie91404) <fatemeh.karampanah@conti.de>
2025-05-14 13:01:32 +0200

    Test

--- BlazorChatApp/Program.cs
+++ BlazorChatApp/Program.cs
@@ -6,16 +6,17 @@ using System;
 using System.Net.Http;
 using BlazorChatApp.services;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor().AddCircuitOptions(options => { options.DetailedErrors = true; });
 
-var baseUrl = Environment.GetEnvironmentVariable("BaseUrl") ?? builder.Configuration["BaseUrl"] ?? "http://localhost:64779";
-Console.WriteLine("Base URL: " + baseUrl);
 
 builder.Services.AddHttpClient("ServerAPI", client =>
 {
+    var baseUrl = Environment.GetEnvironmentVariable("BaseUrl") ?? builder.Configuration["BaseUrl"] ?? "http://localhost:5001";
+    Console.WriteLine("Base URL: " + baseUrl);
     client.BaseAddress = new Uri(baseUrl);
 });
 // ✅ Provide a default HttpClient via DI
@@ -27,13 +28,16 @@ builder.Services.AddDbContext<ChatDbContext>(options =>
     options.UseSqlite("Data Source=chat.db"));
 builder.Services.AddControllers();
 
-builder.Services.AddScoped<SignalRService>();     
-builder.Services.AddScoped<UserService>();         
+builder.Services.AddScoped<SignalRService>();
+builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<AppStatusService>();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(5000); // HTTP only
+    options.ListenAnyIP(5001); // HTTP only
 });
 
 var app = builder.Build();
